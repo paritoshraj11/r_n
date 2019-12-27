ff9aa229a50f80a1e7d00586c147fef6e77c9875
paritosh <paritosh.raj@appstreet.io>
2019-12-27 11:03:41 +0530

    understanding the basics

--- src/app.js
+++ src/app.js
@@ -1,44 +1,10 @@
 const http = require("http");
 const fs = require("fs");
+const route = require("./route");
 
 const PORT = 5000;
 
-const requestListener = (req, res) => {
-  const url = req.url;
-  const method = req.method;
-  console.log("::::::", { url, method });
-  res.setHeader("Content-Type", "text/html");
-  if (url === "/") {
-    res.write(
-      `<html>
-         <head>
-             <title>My App </title>
-         </head>
-         <body>
-           <p>Form test </p>
-           <form action="/message" method="POST"  >
-             <input name="message" />
-             <button type="submit">Submit</button>
-           </form>
-         </body>
-      </html`
-    );
-    return res.end();
-  }
-
-  if (url === "/message" && method === "POST") {
-    // fs.writeFileSync("msg.text", "Dummy data");
-    res.statusCode = 302;
-    res.setHeader("Location", "/"); //set location header to redirect client side to specific url
-    return res.end();
-  }
-
-  res.write(
-    `<html><head><title>My App </title></head><body><h1>this is testing app </h1></body></html`
-  );
-  res.end();
-};
-const server = http.createServer(requestListener);
+const server = http.createServer(route);
 
 const listeningListener = () => {
   console.log(`:: server started to listen at port ${PORT}   `);
---
+++ src/route.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+  res.setHeader("Content-Type", "text/html");
+  if (url === "/") {
+    res.write(
+      `<html>
+           <head>
+               <title>My App </title>
+           </head>
+           <body>
+             <p>Form test </p>
+             <form action="/message" method="POST"  >
+               <input name="message" />
+               <button type="submit">Submit</button>
+             </form>
+           </body>
+        </html`
+    );
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    let chunks = [];
+    req.on("data", chunk => {
+      chunks.push(chunk);
+    });
+    req.on("end", () => {
+      let buffer = Buffer.concat(chunks).toString();
+      fs.writeFileSync("testing1.txt", buffer);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/"); //set location header to redirect client side to specific url
+    return res.end();
+  }
+
+  res.write(
+    `<html><head><title>My App </title></head><body><h1>this is testing app </h1></body></html`
+  );
+  res.end();
+};
+
+module.exports = requestHandler;
